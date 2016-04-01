26ef8e6f766fdd8a5e1836b7e98835a62d5d1200
chenyingmiao <2740433420@qq.com>
2016-04-02 00:00:50 +0800

    one

---
+++ app/src/main/java/com/aode/buyoapp/StartActivity.java
@@ -0,0 +1,64 @@
+package com.aode.buyoapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.aode.buyoapp.business.activity.LoginActivity;
+
+/**
+ * 启动布约app界面
+ *
+ * @author 陈映苗
+ *         created by 10:00 2016-03-31
+ */
+public class StartActivity extends Activity {
+
+    /**
+     * 启动界面中的商家端按钮
+     */
+    private Button btn_start_business;
+    /**
+     * 启动界面的用户端按钮
+     */
+    private Button btn_start_customer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        /**
+         * 商家端按钮和用户端按钮的透明度动画
+         */
+        btn_start_business = (Button) findViewById(R.id.btn_start_business);
+        btn_start_customer = (Button) findViewById(R.id.btn_start_customer);
+        AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(3000);
+        btn_start_business.startAnimation(animation);
+        btn_start_customer.startAnimation(animation);
+
+    }
+
+    /**
+     * 点击商家按钮进入商家端登录界面
+     *
+     */
+    private void startBusiness(View v){
+        startActivity(new Intent(StartActivity.this,LoginActivity.class));
+    }
+
+    /**
+     * 点击用户按钮进入用户端首页界面
+     *
+     */
+    private void startCustomer(View v){
+
+    }
+}
