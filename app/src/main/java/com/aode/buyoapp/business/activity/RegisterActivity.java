3d3c8b9a58327b493c544841c146bbae65644225
ChenYingMiao <2740433420@qq.com>
2016-04-03 19:56:50 +0800

    第二次提交，修改了登录界面，增加了注册界面，加入自定义控件和自定义属性

---
+++ app/src/main/java/com/aode/buyoapp/business/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.aode.buyoapp.business.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.aode.buyoapp.R;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
