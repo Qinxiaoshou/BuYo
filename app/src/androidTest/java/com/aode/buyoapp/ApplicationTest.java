6ea65f846a7fea86fd7a15637967de294ceb4ee3
factorlinebarrel <894470092@qq.com>
2016-04-07 11:14:49 +0800

    w shi leilei

--- app/src/androidTest/java/com/aode/buyoapp/ApplicationTest.java
+++ app/src/androidTest/java/com/aode/buyoapp/ApplicationTest.java
@@ -5,6 +5,7 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * 我 是黎垒
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
