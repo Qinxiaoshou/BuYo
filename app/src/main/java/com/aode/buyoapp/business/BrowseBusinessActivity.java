8a2bf0236299cbbcccbfbe7a76c9b2c3a5cb68e2
chenyingmiao <2740433420@qq.com>
2016-05-06 19:25:44 +0800

    Merge branch 'master' of G:\git\sourceTreeStroge\BuYo with conflicts.

---
+++ app/src/main/java/com/aode/buyoapp/business/BrowseBusinessActivity.java
@@ -0,0 +1,15 @@
+package com.aode.buyoapp.business;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aode.buyoapp.R;
+
+public class BrowseBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_business);
+    }
+}
