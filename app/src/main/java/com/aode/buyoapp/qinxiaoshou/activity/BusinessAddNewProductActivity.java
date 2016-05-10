cf3b3a390d666196623208937a724dc7894c21f3
qinxiaoshou <1031219129@qq.com>
2016-05-10 09:47:47 +0800

    修改一下界面细节

--- app/src/main/java/com/aode/buyoapp/qinxiaoshou/activity/BusinessAddNewProductActivity.java
+++ app/src/main/java/com/aode/buyoapp/qinxiaoshou/activity/BusinessAddNewProductActivity.java
@@ -1,16 +1,20 @@
 package com.aode.buyoapp.qinxiaoshou.activity;
 
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.aode.buyoapp.LL.Home_business;
@@ -19,27 +23,77 @@ import com.aode.buyoapp.LL.bean.Cloth;
 import com.aode.buyoapp.LL.view.IBusinessProductAddView;
 import com.aode.buyoapp.R;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.logging.LogRecord;
+
+
 
 /**
- * 友好商家界面
+ * 添加商品界面
  *
  * @author 覃培周
  * @// FIXME: 2016/4/7
  */
-public class BusinessAddNewProductActivity extends AppCompatActivity {
+public class BusinessAddNewProductActivity extends AppCompatActivity implements IBusinessProductAddView {
     private RecyclerView recyclerView;
     private Toolbar toolbar;
+    private ArrayList<String> mSelectPath;
+    public static final int REQUEST_IMAGE = 2;
+    public String imgUrl;
+    Bitmap bitmap = null;
+
+    EditText et_title;
+    EditText et_size;
+    EditText et_price;
+    EditText et_stock;
+    EditText et_color;
+    EditText et_parttern;
+    ImageView iv_prodct_image;
+    private Cloth cloth;
+    BusinessProductAddPresenter businessProductAddPresenter = new BusinessProductAddPresenter(this);
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String imgOk = bundle.getString("imgOk");
+            if ("imgOk".equals(imgOk)) {
+                System.out.println("***********************:" + bitmap.toString());
+                iv_prodct_image.setImageBitmap(bitmap);
+            } else {
+                System.out.println("图片设置后的更新页面失败");
+            }
+
+        }
+    };
+
     //完成按钮
     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.business_add_product_layout);
+        setContentView(R.layout.business_add_product_content);
+
+        et_title = (EditText) findViewById(R.id.et_title);
+        et_size = (EditText) findViewById(R.id.et_size);
+        et_price = (EditText) findViewById(R.id.et_price);
+        et_stock = (EditText) findViewById(R.id.et_stock);
+        et_color = (EditText) findViewById(R.id.et_color);
+        et_parttern = (EditText) findViewById(R.id.et_parttern);
+        //上传图片
+        iv_prodct_image = (ImageView) findViewById(R.id.iv_prodct_image_add);
+
+
         //步骤一：添加一个FragmentTransaction的实例
-        button = (Button) findViewById(R.id.btn_right_text);
+        button = (Button) findViewById(R.id.btn_right_add);
         //步骤二：用add()方法加上Fragment的对象rightFragment
-        toolbar = (Toolbar) findViewById(R.id.toolbar_business_product_details);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_business_product_details_add);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,101 +102,57 @@ public class BusinessAddNewProductActivity extends AppCompatActivity {
         });
         toolbar.setNavigationIcon(R.drawable.left_arrow);//设置导航栏图标
 
-        recyclerView = (RecyclerView)findViewById(R.id.rv_product_details_view);
-        recyclerView.setVisibility(View.VISIBLE);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new BusinessAddProductDataRecyclerViewAdapter(this,button));
-
-    }
-
-}
-    /**
-     * 商家添加商品信息的适配器
-     *
-     * @author 覃培周
-     * @// FIXME: 2016/4/7
-     */
-    class BusinessAddProductDataRecyclerViewAdapter extends RecyclerView.Adapter<BusinessAddProductDataRecyclerViewAdapter.ViewHolder> implements IBusinessProductAddView {
-
-        private Cloth cloth;
-        private Context mContext;
-        private Button button;
-
-        public BusinessAddProductDataRecyclerViewAdapter(BusinessAddNewProductActivity businessAddNewProductActivity, Button button) {
-            this.mContext = businessAddNewProductActivity;
-            this.button = button;
-        }
-
-        BusinessProductAddPresenter businessProductAddPresenter = new BusinessProductAddPresenter(this);
-
-        //列表页面的布局实现
-        @Override
-        public BusinessAddProductDataRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.business_add_product_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final BusinessAddProductDataRecyclerViewAdapter.ViewHolder holder, int position) {
-            //完成按钮
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    cloth = new Cloth();
-                    cloth.setTitle(holder.et_title.getText().toString().trim());
-                    cloth.setSize(holder.et_size.getText().toString().trim());
-                    cloth.setPrice(Double.valueOf(holder.et_price.getText().toString().trim()));
-                    cloth.setStock(Long.valueOf(holder.et_stock.getText().toString().trim()));
-                    cloth.setColor(holder.et_color.getText().toString().trim());
-                    cloth.setPattern(holder.et_parttern.getText().toString().trim());
-                    cloth.setbId(Home_business.business.getId());
+        //完成按钮
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cloth = new Cloth();
+                cloth.setTitle(et_title.getText().toString().trim());
+                cloth.setSize(et_size.getText().toString().trim());
+                cloth.setPrice(Double.valueOf(et_price.getText().toString().trim()));
+                cloth.setStock(Long.valueOf(et_stock.getText().toString().trim()));
+                cloth.setColor(et_color.getText().toString().trim());
+                cloth.setPattern(et_parttern.getText().toString().trim());
+                cloth.setbId(Home_business.business.getId());
+                if (cloth != null) {
                     businessProductAddPresenter.ProductAdd();
+                } else {
+                    Toast.makeText(getApplicationContext(), "请输入商品信息", Toast.LENGTH_LONG).show();
                 }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return 1;
-        }
-
-        @Override
-        public Cloth getProduct() {
-            return cloth;
-        }
-
-        /**
-         * 出现不能添加商品问题
-         */
-        @Override
-        public void toMainActivity() {
-            Toast.makeText(mContext, "添加成功", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void showFailedError() {
-            Toast.makeText(mContext, "添加失败", Toast.LENGTH_SHORT).show();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            final EditText et_title;
-            final EditText et_size;
-            final EditText et_price;
-            final EditText et_stock;
-            final EditText et_color;
-            final EditText et_parttern;
-
-            public ViewHolder(View view) {
-                super(view);
-                et_title = (EditText) view.findViewById(R.id.et_title);
-                et_size = (EditText) view.findViewById(R.id.et_size);
-                et_price = (EditText) view.findViewById(R.id.et_price);
-                et_stock = (EditText) view.findViewById(R.id.et_stock);
-                et_color = (EditText) view.findViewById(R.id.et_color);
-                et_parttern = (EditText) view.findViewById(R.id.et_parttern);
-                mView = view;
             }
-        }
+        });
+
+
+        iv_prodct_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public Cloth getProduct() {
+        return cloth;
+    }
+
+    /**
+     * 出现不能添加商品问题
+     */
+    @Override
+    public void toMainActivity() {
+        Toast.makeText(getApplicationContext(), "添加成功", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showFailedError() {
+        Toast.makeText(getApplicationContext(), "添加失败", Toast.LENGTH_SHORT).show();
+    }
+}
+
+
--- app/src/main/java/com/aode/buyoapp/qinxiaoshou/activity/BusinessUpdateProductMessagesActivity.java
+++ app/src/main/java/com/aode/buyoapp/qinxiaoshou/activity/BusinessUpdateProductMessagesActivity.java
@@ -3,51 +3,62 @@ package com.aode.buyoapp.qinxiaoshou.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.aode.buyoapp.LL.Presenter.BusinessProductChangePresenter;
 import com.aode.buyoapp.LL.bean.Cloth;
+import com.aode.buyoapp.LL.view.IBusinessProductChangeView;
 import com.aode.buyoapp.R;
-import com.aode.buyoapp.qinxiaoshou.fragment.BusinessUpdateProductDataFragment;
 
 
 /**
  * 修改商品信息
+ *
  * @author 覃培周
  * @// FIXME: 2016/4/7
  */
-public class BusinessUpdateProductMessagesActivity extends FragmentActivity {
+public class BusinessUpdateProductMessagesActivity extends FragmentActivity implements IBusinessProductChangeView {
 
-    private BusinessUpdateProductDataFragment businessUpdateProductDataFragment;
     private Toolbar toolbar;
     private TextView toolbarTitle;
     private RadioGroup rg_h;
     private Cloth cloth;
     private Button button;
+    BusinessProductChangePresenter businessProductChangePresenter = new BusinessProductChangePresenter(this);
+
+
+    private EditText et_title;
+    private EditText et_size;
+    private EditText et_price;
+    private EditText et_stock;
+    private EditText et_color;
+    private EditText et_parttern;
+    private Cloth cloth2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.business_add_product_layout);
+        setContentView(R.layout.business_update_product_content);
         //获取前面页面传递过来的数据
         Intent intent = this.getIntent();
-        cloth=(Cloth)intent.getSerializableExtra("cloth");
+        cloth = (Cloth) intent.getSerializableExtra("cloth");
+        button = (Button) findViewById(R.id.btn_right_update);
 
-        button = (Button) findViewById(R.id.btn_right_text);
+        et_title = (EditText) findViewById(R.id.et_title);
+        et_size = (EditText) findViewById(R.id.et_size);
+        et_price = (EditText) findViewById(R.id.et_price);
+        et_stock = (EditText) findViewById(R.id.et_stock);
+        et_color = (EditText) findViewById(R.id.et_color);
+        et_parttern = (EditText) findViewById(R.id.et_parttern);
 
-
-
-        toolbarTitle = (TextView) findViewById(R.id.tv_g_add_product_title);
-        //步骤一：添加一个FragmentTransaction的实例
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        //步骤二：用add()方法加上Fragment的对象rightFragment
-        businessUpdateProductDataFragment = new BusinessUpdateProductDataFragment(cloth,button);
-        toolbar = (Toolbar)findViewById(R.id.toolbar_business_product_details);
+        toolbarTitle = (TextView) findViewById(R.id.tv_g_update_product_title);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_business_product_details_update);
         rg_h = (RadioGroup) findViewById(R.id.rg_h_open_permission);
         toolbar.setNavigationIcon(R.drawable.left_arrow);//设置导航栏图标
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -59,8 +70,76 @@ public class BusinessUpdateProductMessagesActivity extends FragmentActivity {
         rg_h.setVisibility(View.INVISIBLE);
         toolbarTitle.setText("修改商品信息");
 
-        transaction.add(R.id.fl_g_framelayout, businessUpdateProductDataFragment).commit();
+
+        et_title.setText(cloth.getTitle());
+        et_size.setText(cloth.getSize());
+        et_price.setText(cloth.getPrice() + "");
+        et_stock.setText(cloth.getStock() + "");
+        et_color.setText(cloth.getColor());
+        et_parttern.setText(cloth.getPattern());
+        //完成按钮
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    cloth2 = new Cloth();
+                    Long id = cloth.getId();
+                    if (id != null) {
+                        cloth2.setId(id);
+                    }
+                    if (et_title.getText().toString().trim() != null) {
+                        cloth2.setTitle(et_title.getText().toString().trim());
+                    } else {
+                        Toast.makeText(getBaseContext(), "布匹标题不能为空", Toast.LENGTH_SHORT).show();
+                    }
+
+                    if (et_size.getText().toString().trim() != null) {
+                        cloth2.setSize(et_size.getText().toString().trim());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "布匹尺寸不能为空 ", Toast.LENGTH_SHORT).show();
+                    }
+                    if (Double.valueOf(et_price.getText().toString().trim()) != null) {
+                        cloth2.setPrice(Double.valueOf(et_price.getText().toString().trim()));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "布匹价格不能为空", Toast.LENGTH_SHORT).show();
+                    }
+                    if (Long.valueOf(et_stock.getText().toString().trim()) != null) {
+                        cloth2.setStock(Long.valueOf(et_stock.getText().toString().trim()));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "库存量不能为空 ", Toast.LENGTH_SHORT).show();
+                    }
+                    if (et_color.getText().toString().trim() != null) {
+                        cloth2.setColor(et_color.getText().toString().trim());
+                    }
+
+                    if (et_parttern.getText().toString().trim() != null) {
+                        cloth2.setPattern(et_parttern.getText().toString().trim());
+                    }
+                    if (cloth.getbId() != null) {
+                        cloth2.setbId(cloth.getbId());
+                    }
+                    businessProductChangePresenter.ProductChange();
+                }catch (Exception e){
+                }
+            }
+        });
 
 
     }
+
+    @Override
+    public Cloth getProduct() {
+        return cloth2;
+    }
+
+    @Override
+    public void toMainActivity() {
+        Toast.makeText(getApplicationContext(),"修改成功",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showFailedError() {
+        Toast.makeText(getApplicationContext(),"修改失败",Toast.LENGTH_SHORT).show();
+    }
+
 }
