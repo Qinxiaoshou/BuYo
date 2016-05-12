b229c75aeb02cdc76dac4d5ce10078ad9656dfb4
ChenYingMiao <2740433420@qq.com>
2016-05-12 16:15:30 +0800

    增加了商家店铺详情界面

--- app/src/main/java/com/aode/buyoapp/business/BrowseBusinessActivity.java
+++ app/src/main/java/com/aode/buyoapp/business/BrowseBusinessActivity.java
@@ -1,11 +1,14 @@
 package com.aode.buyoapp.business;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.aode.buyoapp.R;
+import com.aode.buyoapp.business.activity.BusinessDetailActivity;
 
 /**
  * 布约app厂商一览界面
@@ -13,9 +16,10 @@ import com.aode.buyoapp.R;
  * @author 陈映苗
  * @// FIXME: 2016/5/7
  */
-public class BrowseBusinessActivity extends AppCompatActivity {
+public class BrowseBusinessActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView iv_browse_business_back;
+    private RelativeLayout rl_browse_1,rl_browse_2,rl_browse_3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +31,27 @@ public class BrowseBusinessActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        rl_browse_1 = (RelativeLayout)findViewById(R.id.rl_browse_1);
+        rl_browse_2 = (RelativeLayout)findViewById(R.id.rl_browse_2);
+        rl_browse_3 = (RelativeLayout)findViewById(R.id.rl_browse_3);
+        rl_browse_1.setOnClickListener(this);
+        rl_browse_2.setOnClickListener(this);
+        rl_browse_3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.rl_browse_1:
+                startActivity(new Intent(BrowseBusinessActivity.this, BusinessDetailActivity.class));
+                break;
+            case R.id.rl_browse_2:
+                startActivity(new Intent(BrowseBusinessActivity.this, BusinessDetailActivity.class));
+                break;
+            case R.id.rl_browse_3:
+                startActivity(new Intent(BrowseBusinessActivity.this, BusinessDetailActivity.class));
+                break;
+        }
     }
 }
