f9111b6c58abcc0476f6d487e3d8463c8929aa0c
liang <929422167@qq.com>
2016-05-15 01:16:58 +0800

    主页版本3 关于我们

--- app/src/main/java/com/aode/buyoapp/LL/Homepage/Person_Homepage2.java
+++ app/src/main/java/com/aode/buyoapp/LL/Homepage/Person_Homepage2.java
@@ -57,7 +57,7 @@ public class Person_Homepage2 extends Fragment implements ViewPager.OnPageChange
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view2 = inflater.inflate(R.layout.fragment_business_homepage2, container, false);
+        view2 = inflater.inflate(R.layout.fragment_person_homepage2, container, false);
 
         init();
 
